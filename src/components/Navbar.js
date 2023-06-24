996821d54b1d11f58a9b8022fb88fad310328548
Karely3412 <karely@devpipeline.com>
2023-06-24 15:28:11 -0600

    created navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function Navbar() {
+  return (
+    <div className="navbar-container">
+      <Link to="/" className="nav-logo"></Link>
+    </div>
+  );
+}
+
+export default Navbar;
